1b259d4f35a7f9987ff7e9532c06eafc1928d0c8
Jez Cordonnier <jez.cordonnier@fridaymediagroup.com>
2017-01-11 14:38:50 +0000

    Rolled back to master and change how git pulls down files

--- install-ansible.sh
+++ install-ansible.sh
@@ -32,19 +32,14 @@ which ansible > /dev/null 2>&1
 if [ $? -eq 1 ]; then
 
   # echo "Installing Ansible build dependencies."
-  # $ANSIBLE_DEBUG=1
   # if [ -z $ANSIBLE_DEBUG ]; then
   #   apt-get -qq --assume-yes update > /dev/null 2>&1
-  #   apt-get -qq --assume-yes install build-essential libssl-dev libffi-dev
   #   apt-get -qq --assume-yes install git python-pip python-setuptools python-dev python-paramiko python-yaml python-jinja2 python-httplib2 python-passlib python-six python-ecdsa > /dev/null 2>&1
-  #   #pip install --upgrade setuptools pip
   #   pip install cryptography
   # else
-    apt-get --assume-yes update
-    apt-get --assume-yes install build-essential autoconf libtool pkg-config libssl-dev libffi-dev unzip python-dev libxml2-dev libxslt1-dev zlib1g-dev python-opengl python-imaging python-pyrex python-pyside.qtopengl qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3
-    apt-get --assume-yes install git python-pip python-setuptools python-paramiko python-yaml python-jinja2 python-httplib2 python-passlib python-six python-ecdsa
-    #pip install --upgrade setuptools pip
-    pip install cryptography
+  #   apt-get --assume-yes update
+  #   apt-get --assume-yes install git python-pip python-setuptools python-dev python-paramiko python-yaml python-jinja2 python-httplib2 python-passlib python-six python-ecdsa
+  #   pip install cryptography
   # fi
 
   if [ -z $branch ] && [ ! -z $ANSIBLE_BRANCH ]; then
@@ -64,29 +59,29 @@ if [ $? -eq 1 ]; then
   if [ ! -d $ansible_dir ]; then
     echo "Cloning Ansible."
     if [ -z $ANSIBLE_DEBUG ]; then
-
-      wget https://github.com/ansible/ansible/archive/stable-1.9.zip -O /usr/local/lib/ansible.zip
-      unzip $ansible_dir/ansible.zip -d $ansible_dir/ansible
+      git clone --quiet --recursive https://github.com/ansible/ansible.git $ansible_dir > /dev/null 2>&1
+      cd $ansible_dir; git checkout ANSIBLE_STABLE_BRANCH --quiet
     else
-      wget https://github.com/ansible/ansible/archive/stable-1.9.zip -O /usr/local/lib/ansible.zip
-      unzip $ansible_dir/ansible.zip -d $ansible_dir/ansible
+      git clone --recursive https://github.com/ansible/ansible.git $ansible_dir
+      cd $ansible_dir; git checkout ANSIBLE_STABLE_BRANCH
     fi
   fi
 
-  # if [ -z $checkout ] && [ ! -z $ANSIBLE_CHECKOUT ]; then
-  #   echo "Setting checkout target from environment."
-  #   checkout=$ANSIBLE_CHECKOUT
-  # fi
+  if [ -z $checkout ] && [ ! -z $ANSIBLE_CHECKOUT ]; then
+    echo "Setting checkout target from environment."
+    checkout=$ANSIBLE_CHECKOUT
+  fi
 
-  # if [ ! -z $checkout ]; then
-  #   echo "Checking out '$checkout'."
-  #   cd $ansible_dir
-  #   if [ -z $ANSIBLE_DEBUG ]; then
-  #     git checkout $checkout --quiet
-  #   else
-  #     git checkout $checkout
-  #   fi
-  # fi
+  if [ ! -z $checkout ]; then
+    echo "Checking out '$checkout'."
+
+    cd $ansible_dir
+    if [ -z $ANSIBLE_DEBUG ]; then
+      git checkout ANSIBLE_STABLE_BRANCH --quiet
+    else
+      git checkout ANSIBLE_STABLE_BRANCH
+    fi
+  fi
 
   echo "Running setups tasks for Ansible."
   cd $ansible_dir
